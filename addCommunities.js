b87d3680f21e778408cef237da4593a532464fca
benstratton6 <53532760+benstratton6@users.noreply.github.com>
2021-05-21 15:16:33 +0200

    Update addCommunities.js

--- addCommunities.js
+++ addCommunities.js
@@ -228,6 +228,7 @@ class AddCommunities {
         const routeDistances = this.routeDistances; 
         console.log("HERE"); 
         console.log(routeDistances); 
+        console.log(routeDistances["Chilikwazi"]);
         const allStats = this.statsData; 
         const allStatsKeys = Object.keys(allStats); 
         const text = this.textFromatterOutreach; 
@@ -246,7 +247,7 @@ class AddCommunities {
         var distanceFromRHP = routeDistances[nameSplit[0]]; 
         console.log(nameSplit[0]); 
         console.log("Inside onEachFunction");
-        console.log("Distance from RHP: " + distanceFromRHP); 
+        console.log("Distance from RHP: " + distanceFromRHP.toString()); 
         //console.log("name :" + name); 
         //console.log("letters :" + letters); 
         //console.log("routes : " + this.routeDistances);
